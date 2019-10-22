285e7882658bceffd6507104ca28e2d77037a4aa
KevinFrazier <kfraz001@ucr.edu>
2019-10-22 14:49:33 -0700

    added react-navigation to mobile app

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text, Button} from 'react-native';
+
+import{styles} from '../styles/styles';
+
+export default class Main extends Component{
+
+    render(){
+
+        return(
+            <View style={styles.center}>
+                <Text> Main Menu </Text>
+                <Button title = 'Go to Feed' onPress = {()=> this.props.navigation.navigate('FeedRoute')} />
+            </View>
+
+        )
+    }
+
+}
