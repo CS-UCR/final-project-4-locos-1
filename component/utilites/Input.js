71617d285e3f1ba8daacabd64b8ff9158952ea7f
Francisco Gallego <fgall002@ucr.edu>
2019-10-23 15:42:31 -0700

    Folder contais utility function for input dynamic function  and input template

---
+++ component/utilites/Input.js
@@ -0,0 +1,102 @@
+import React, { useReducer, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+
+const INPUT_CHANGE = 'INPUT_CHANGE';
+const INPUT_BLUR = 'INPUT_BLUR';
+
+const inputReducer = (currentState, action) => {
+  switch (action.type) {
+    case INPUT_CHANGE:
+      return {
+        ...currentState,
+        value: action.value,
+        isValid: action.isValid
+      };
+    case INPUT_BLUR:
+      return {
+        ...currentState,
+        touched: true
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = props => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue ? props.initialValue : '',
+    isValid: false,
+    touched: false
+  });
+
+  const { onInputChange, id } = props;
+
+  useEffect(() => {
+    if (inputState.touched) {
+      onInputChange(id, inputState.value, inputState.isValid);
+    }
+  }, [inputState, onInputChange, id]);
+
+  const textChangeHandler = text => {
+    const emailRegex = /[^@]+@[^\.]+\..+/;
+    let isValid = true;
+    if (props.required && text.trim().length === 0) {
+      isValid = false;
+    }
+    if (props.email && !emailRegex.test(text.toLowerCase())) {
+      isValid = false;
+    }
+    if (props.minLength != null && text.length < props.minLength) {
+      isValid = false;
+    }
+    dispatch({ type: INPUT_CHANGE, value: text, isValid: isValid });
+  };
+
+  const lostFocusHandler = () => {
+    dispatch({ type: INPUT_BLUR });
+  };
+
+  return (
+    <View style={styles.formControl}>
+      <Text style={styles.label}>{props.label}</Text>
+      <TextInput
+        {...props}
+        style={styles.input}
+        value={inputState.value}
+        onChangeText={textChangeHandler}
+        onBlur={lostFocusHandler}
+      />
+      {!inputState.isValid && inputState.touched && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>{props.errorText}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  formControl: {
+    width: '100%'
+  },
+  label: {
+    // fontFamily: 'open-sans-bold',
+    marginVertical: 8
+  },
+  input: {
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1
+  },
+  errorContainer: {
+    marginVertical: 5
+  },
+  errorText: {
+    // fontFamily: 'open-sans',
+    color: 'red',
+    fontSize: 13
+  }
+});
+
+export default Input;
---
+++ component/utilites/InputLayout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+
+
+const AuthLayout = props => {
+  return <View style={{...styles.layout, ...props.style}}>{props.children}</View>;
+};
+
+const styles = StyleSheet.create({
+  layout: {
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: 'white'
+  }
+});
+
+export default AuthLayout;
