14a4eeb5b744d254535cba1a619cc7241762bb94
KevinFrazier <kfraz001@ucr.edu>
2019-10-23 16:05:23 -0700

    added Francisco's merge

--- src/components/myMap.js
+++ src/components/myMap.js
@@ -1,58 +1,43 @@
-/*
+import React from 'react';
+import MapView, {Permissions} from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
 
-import React, { Component } from "react";
-import {StyleSheet, Text, View } from 'react-native';
+export default class App extends React.Component {
 
-export default class myMap extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    this.googleMapRef = React.createRef();
+  state = {
+    latitude: null,
+    longitude: null
+  }
+
+
+  //ASK USER IF HE/SHE ALLOWS TO ACCES DEVICES LOCATION
+  async componentDidMount(){
+      const {status} = await Location.hasServicesEnabledAsync()
+      //const {status} = await Permissions.getAsync(Permissions.LOCATION)
+    if(status != 'granted'){
+      const response = await Location.requestPermissionsAsync()
+      //const response = await Permissions.askAsync(Permissions.LOCATION)
     }
+  }
 
-    componentDidMount() {
-        const googleMapScript = document.createElement("script");
-        googleMapScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDEkGijSUn59Urd96tftpKsizYGqkUaMFQ&libraries=places`;
-        window.document.body.appendChild(googleMapScript);
-        googleMapScript.addEventListener("load", () => {
-            this.googleMap = this.createGoogleMap();
-            this.marker = this.createMarker();
-        });
-    }
-    
-    createGoogleMap = () =>
-    new window.google.maps.Map(this.googleMapRef.current, {
-        zoom: 16,
-        center: {
-            lat: 43.642567,
-            lng: -79.387054
-        },
-        disableDefaultUI: true
-    });
-    
-    createMarker = () =>
-    new window.google.maps.Marker({
-        position: { lat: 43.642567, lng: -79.387054 },
-        map: this.googleMap
-    });
-    
-    render() {
-        return (
-        <div>
-        <Text>
-        I am a very simple card. I am good at containing small bits of
-        information. I am convenient because I require little markup
-        to use effectively. I am similar to what is called a panel in
-        other frameworks.
-        </Text>
-        
-        <div
-        ref={this.googleMapRef}
-        id="google-map"
+  
+
+  render() {
+    return (
+        <MapView style={styles.mapStyle}
+          Region = {{
+            latitudeDelta: 0.0922,
+            longitudeDelta:  0.0421,
+          }}
         />
-        </div>
-        );
-        }
-    }
+    );
+  }
+}
 
-*/
+const styles = StyleSheet.create({
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
