e8b85c92f3c562b307fcd93c2ae76651b35c6597
Francisco Gallego <fgall002@ucr.edu>
2019-10-27 21:44:58 -0700

    Moved all navigation acitivies into this folder

---
+++ src/Navigation/navigation.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {createAppContainer, createSwitchNavigator}  from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack'
+
+
+import AuthenticationMenu from '../authentication/AuthenticationMenu'
+import SignUp from '../authentication/User/SignUpScreen'
+import Login from '../authentication/User/LoginScreen'
+
+import Title from '../components/Title'
+import Feed from '../components/Feed'
+import Menu from '../components/Main'
+import myMap from '../components/myMap'
+
+const Features = createStackNavigator(
+    {
+        MainRoute: Menu,
+        TitleRoute: Title,
+        FeedRoute: Feed,
+        MapRoute: myMap,
+    }
+)
+
+
+
+const Authentication = createStackNavigator(
+    {
+        AuthenticationMenu: AuthenticationMenu,
+        SignUp: SignUp,
+        Login:Login
+    }
+)
+
+const MainNavigator = createSwitchNavigator({
+    Auth: Authentication,
+    Features: Features
+})
+
+
+export default createAppContainer(MainNavigator)
