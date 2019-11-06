d9928caf9bf6b50d1c76998e3d5a908b5613c874
Francisco Lagos Vilaboa <flagosvilaboa@gmail.com>
2019-11-06 02:46:42 -0800

    maps show blue marker with users location - map still does not start at users location

--- src/components/myMap.js
+++ src/components/myMap.js
@@ -1,51 +1,67 @@
 import React from 'react';
-import MapView, {Permissions} from 'react-native-maps';
+import MapView, {Permissions, Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import * as Location from 'expo-location';
 
 export default class myMap extends React.Component {
 
-  state = {
-    latitude: null,
-    longitude: null
-  }
+  // //ASK USER IF HE/SHE ALLOWS TO ACCES DEVICES LOCATION
+  // async componentDidMount(){
+  //   const {status} = await Location.hasServicesEnabledAsync()
+  //   //const {status} = await Permissions.getAsync(Permissions.LOCATION)
+  //   if(status != 'granted'){
+  //     const response = await Location.requestPermissionsAsync()
+  //     //const response = await Permissions.askAsync(Permissions.LOCATION)
+  //   }
+  // }  
 
-
-  //ASK USER IF HE/SHE ALLOWS TO ACCES DEVICES LOCATION
-  async componentDidMount(){
-      const {status} = await Location.hasServicesEnabledAsync()
-      //const {status} = await Permissions.getAsync(Permissions.LOCATION)
-    if(status != 'granted'){
-      const response = await Location.requestPermissionsAsync()
-      //const response = await Permissions.askAsync(Permissions.LOCATION)
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      error: null
     }
   }
 
-  
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position =>{
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        })
+      },
+      error => this.setState({error: error.message}),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 10000}
+    )
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <MapView style={styles.mapStyle}
-          Region = {{
+        <MapView
+          showsUserLocation={true} 
+          style={styles.mapStyle}
+          initialRegion={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
             latitudeDelta: 0.0922,
-            longitudeDelta:  0.0421,
-          }}
-        />
+            longitudeDelta: 0.0421
+          }}/>
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   mapStyle: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 90,
+    height: Dimensions.get('window').height - 80,
   },
 });
