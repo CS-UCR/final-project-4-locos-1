53a89f7fc4acf4b8b22f5eef48a2bbddc9472cba
Francisco Lagos Vilaboa <flagosvilaboa@gmail.com>
2019-11-05 23:17:50 -0800

    centered the map

--- src/components/myMap.js
+++ src/components/myMap.js
@@ -25,19 +25,27 @@ export default class myMap extends React.Component {
 
   render() {
     return (
+      <View style={styles.container}>
         <MapView style={styles.mapStyle}
           Region = {{
             latitudeDelta: 0.0922,
             longitudeDelta:  0.0421,
           }}
         />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   mapStyle: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('window').height - 90,
   },
 });
