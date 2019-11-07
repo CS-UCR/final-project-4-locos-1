9cdede027278f764d7aeac45a4fef0016c71184c
Francisco Lagos Vilaboa <flagosvilaboa@gmail.com>
2019-11-06 22:31:14 -0800

    user is able to start start the map centered in users location when opening the map

--- src/components/myMap.js
+++ src/components/myMap.js
@@ -5,33 +5,27 @@ import * as Location from 'expo-location';
 
 export default class myMap extends React.Component {
 
-  // //ASK USER IF HE/SHE ALLOWS TO ACCES DEVICES LOCATION
-  // async componentDidMount(){
-  //   const {status} = await Location.hasServicesEnabledAsync()
-  //   //const {status} = await Permissions.getAsync(Permissions.LOCATION)
-  //   if(status != 'granted'){
-  //     const response = await Location.requestPermissionsAsync()
-  //     //const response = await Permissions.askAsync(Permissions.LOCATION)
-  //   }
-  // }  
-
   constructor(props) {
     super(props);
     this.state = {
       latitude: 0,
       longitude: 0,
-      error: null
     }
   }
 
-  componentDidMount(){
+  async componentDidMount(){
+   
+    const {status} = await Location.hasServicesEnabledAsync()
+    if(status != 'granted'){
+      const response = await Location.requestPermissionsAsync()
+    }
+
     navigator.geolocation.getCurrentPosition(
       position =>{
         this.setState({
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
-          error: null
-        })
+        });
       },
       error => this.setState({error: error.message}),
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 10000}
@@ -42,23 +36,22 @@ export default class myMap extends React.Component {
     return (
       <View style={styles.container}>
         <MapView
-          showsUserLocation={true} 
           style={styles.mapStyle}
-          initialRegion={{
+          showsUserLocation={true}
+          region={{
             latitude: this.state.latitude,
             longitude: this.state.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01 
           }}/>
-      </View>
+      </View>                                                                                                                                                                                                                         
     );
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    alignItems: 'center',    
   },
   mapStyle: {
     width: Dimensions.get('window').width,
