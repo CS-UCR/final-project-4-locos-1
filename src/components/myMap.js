2791ec52a317f7c17db3f5cd7f96415ec12f9bbe
Francisco Lagos Vilaboa <flagosvilaboa@gmail.com>
2019-11-08 13:40:47 -0800

    User is able to create polygons(Study Spaces), There are errors to fix but it is mainly done

--- src/components/myMap.js
+++ src/components/myMap.js
@@ -1,25 +1,80 @@
 import React from 'react';
-import MapView, {Permissions, Marker, Polygon, ProviderPropType,} from 'react-native-maps';
+import MapView, {Polygon, ProviderPropType, MAP_TYPES,} from 'react-native-maps';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
-import * as Location from 'expo-location';
 
+//TO DO:
+/*
+1.-Be able to make polygons stay on the map everytime it is reopened (firebase?)                              - PO
+2.-Be able to make polygons by using only two points (or at least 4 only)                                     - P1
+3.-Be able to fix distortions on the polygons                                                                 - P4
+4.-Be able to delete polygons                                                                                 - P2
+5.-Try to create a button to start creating polyongs rather than just starting when clicking in the map       - P3
+6.-Have a button to go back to users location                                                                 - P5
+7.-Edit colors                                                                                                - P6
+*/
+
+const { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height; 
+const LATITUDE = 33.9737;       //UC RIVERSIDE LATITUDE
+const LONGITUDE = -117.3281;    //UC RIVERSIDE LONGITUDE
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
 
 export default class myMap extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      latitude: 33.9737,
-      longitude: 117.3281,
-      polygons:[]    
+      region:{
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      polygons: [],
+      editing: null,
     }
   }
+  
+  finish(){
+    const{polygons, editing} = this.state;
+    this.setState({
+      polygons: [...polygons, editing],
+      editing: null,
+    });
+  }
+
+  onPress(e){
+    const{editing} = this.state;
+    if(!editing){
+      this.setState({
+        editing: {
+          id: id++,
+          coordinates: [e.nativeEvent.coordinate],
+          holes:[],
+        },
+      });
+    } else{
+      this.setState({
+        editing:{
+          ...editing,
+          coordinates: [...editing.coordinates, e.nativeEvent.coordinate],
+        },
+      });
+    } 
+  }
+
   componentDidMount(){
     navigator.geolocation.getCurrentPosition(
       position =>{
         this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+          initialRegion:{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA
+          }
         });
       },
       error => this.setState({error: error.message}),
@@ -27,60 +82,72 @@ export default class myMap extends React.Component {
     )
   }
 
-  _onPress = () =>{
-    
-  }
-
   render() {
     return (
-      <View style={styles.mapStyle}>
+      <View style={styles.container}>
         <MapView
+          provider={this.props.provider}
           style={styles.mapStyle}
+          //mapType = {MAP_TYPES.HYBRID}       //CHOOSE THE TYPE OF MAP YOU WANT TO USE
           showsUserLocation={true}
-          showsMyLocationButton={true}
-          region={{
-            latitude: this.state.latitude,
-            longitude: this.state.longitude,
-            latitudeDelta: 0.09,
-            longitudeDelta: 0.09 }}>
-          <Polygon 
-            coordinates={[
-              {name: '1', latitude: this.state.latitude + 0.01, longitude: this.state.longitude - 0.01},
-              {name: '2', latitude: this.state.latitude + 0.01, longitude: this.state.longitude + 0.01},
-              {name: '3', latitude: this.state.latitude - 0.01, longitude: this.state.longitude + 0.01},
-              {name: '4', latitude: this.state.latitude - 0.01, longitude: this.state.longitude - 0.01},
-            ]}
-            strokeWidth={2}
-            strokeColor={'red'}
-            fillColor={'hsla(360, 100%, 50%, 0.5)'}>
-          </Polygon> 
+          initialRegion={this.state.initialRegion}
+            onPress={e => this.onPress(e)}
+          >
+            {this.state.polygons.map(polygon => (
+              <Polygon
+                key={polygon.id}
+                coordinates={polygon.coordinates}
+                holes={polygon.holes}
+                strokeColor={'red'}
+                fillColor={'hsla(240, 100%, 50%, 0.5)'}
+                strokeColor={1}
+              />
+            ))}
+            {this.state.editing && (
+              <Polygon
+                key={this.state.editing.id}
+                coordinates={this.state.editing.coordinates}
+                holes={this.state.editing.holes}
+                strokeColor={'red'}
+                fillColor={'hsla(240, 100%, 50%, 0.5)'}
+                strokeColor={1}
+              />
+            )}
         </MapView>
         <View style={styles.buttonContainerStyle}>
-          <TouchableOpacity 
-            style={styles.buttonBoxStyle}
-            onPress={this._onPress}>
-            <Text style={styles.buttonTextStyle}>
-              New Study Space
-            </Text>
-          </TouchableOpacity> 
-        </View> 
+          {this.state.editing && (
+            <TouchableOpacity
+              onPress={() => this.finish()}
+              style={[styles.secondButtonStyle, styles.ButtonStyle]}
+            >
+              <Text>Finish</Text>
+            </TouchableOpacity>
+          )}
+        </View>
       </View>                                                                                                                                                                                                    
     );
   }
 }
 
+myMap.propTypes ={
+  provider: ProviderPropType,
+};
+
 const styles = StyleSheet.create({
+  container:{
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
   mapStyle: {
-    flex: 1
-    //  width: Dimensions.get('window').width,
-    //  height: Dimensions.get('window').height - 105,
+    ...StyleSheet.absoluteFillObject,
   },
   buttonContainerStyle:{
     position: 'absolute',
     alignSelf: 'center',
-     bottom: 20,
+    bottom: 20,
   },
-  buttonBoxStyle:{
+  buttonStyle:{
     alignSelf:'center',
     alignItems: 'center',
     backgroundColor: '#hsla(60, 100%, 50%, 0.5)',
@@ -88,11 +155,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 12,
     width: 100,
-
   },
-  buttonTextStyle:{
-    textAlign: 'center',
-    fontWeight: '900',
-    color: 'black'
-  },
+  secondButtonStyle:{
+    backgroundColor: '#hsla(60, 100%, 50%, 0.5)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,    
+  }
 });
