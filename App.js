cd50aba7d20755558421f77b458f81b08e281800
Francisco Gallego <fgall002@ucr.edu>
2019-11-12 19:24:37 -0800

    Added redex for storage use

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View,} from 'react-native';
 import {credentials} from './src/config';
-import AuthenticationMenu from './src/Navigation/navigation'
+import AuthenticationMenu from './src/Navigation/navigation';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import UserAuthenticationReducer from './src/authentication/store/Reducer/auth'
+
 
 
 
 credentials();
 
+const rootReducer = combineReducers({
+  userAuth: UserAuthenticationReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+
 export default function App(){
   return (
-    <AuthenticationMenu/>
+    <Provider store={store}>
+      <AuthenticationMenu/>
+    </Provider>
   )
 }
