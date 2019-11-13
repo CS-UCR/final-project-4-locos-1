cf5dbd0fbfda0068c3e43ee340a3601d60f8e7f9
Ulyana Tkachenko <utkac001@ucr.edu>
2019-11-12 18:41:02 -0800

    Working UI For Creating A Workspace Without Add Users Option 	- TODO: add ability to add users based on email/username 	- TODO: add more settings for creating a workspace

---
+++ src/components/CreateWorkspace.js
@@ -0,0 +1,141 @@
+import React, {Component} from 'react';
+import {View, Text, Button, Platform, TouchableHighlight, StyleSheet} from 'react-native';
+import t from 'tcomb-form-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        //alignItems: 'center',
+        backgroundColor: '#FFE9EC',
+        padding: 25,
+    },
+    pageTitleText:{
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    createWorkspaceButton: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#feccc1',
+        borderWidth: 2,
+        borderColor: "grey"
+    },
+    createWorkspaceButtonText: {
+        textAlign: 'center',
+        padding: 5,
+        fontSize: 20,
+        color: '#505050',
+    },
+});
+
+const Form = t.form.Form;
+
+const WorkspaceI = t.struct({
+    workspaceName: t.String,
+    addUsers: t.maybe(t.Boolean),
+})
+
+const formStyles = {
+    ...Form.stylesheet,
+    formGroup: {
+        normal: {
+          marginBottom: 5
+        },
+        error: {
+          marginBottom: 5
+        }
+    },
+    controlLabel: {
+      normal: {
+        fontSize: 15,
+        marginBottom: 5,
+      },
+      error: {
+        color: 'red',
+        fontSize: 15,
+        marginBottom: 5,
+      }
+    },
+    errorBlock: {
+        fontSize: 12,
+        marginBottom: 0,
+        color: 'red',
+    },
+}
+
+const formOptions = {
+    fields: {
+        workspaceName:{
+            label: 'Workspace Name',
+            error: 'Please input a workspace name',
+        },
+        addUsers:{
+            label: 'Add Users',
+        },
+    },
+    stylesheet: formStyles,
+  };
+
+export default class CreateWorkspace extends Component{
+
+    constructor(props){
+        super(props);
+    
+        this.state={
+            info: null,
+        }
+    }
+
+    handleSubmit = () => {
+        const value = this._form.getValue();
+        if(value){
+            info = value
+            alert("Your info has been updated, " + value.firstname + "!")
+        }else{
+            alert("Please correct the errors")
+        }
+        console.log('value: ', value);
+    }
+    //TODO:Update info into firebase
+    updateFirebase = () => {
+        if(this.state.info){
+            //Constants to be updated into firebase
+            const info = this.state.info
+            const   workspaceName = info.workspaceName,
+                    addUsers = info.addUsers
+        }else{
+            alert("Updating firebase with invalid info...")
+        }
+    }
+    
+    onPressCreateWorkspaceButton = () => {
+        this.handleSubmit()
+        if(this.state.info){
+            this.updateFirebase()
+        }
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <View style={{height: 30}}>
+                    <Text style={styles.pageTitleText}>Create Workspace</Text>
+                </View>
+                <View style={{height: 40}}></View>
+                <View style={{height: 400}}>
+                    <Form 
+                        ref={c => this._form = c}
+                        type={WorkspaceI}
+                        options={formOptions} />
+                </View>
+                <TouchableHighlight onPress={this.onPressCreateWorkspaceButton} underlayColor="white">    
+                    <View style={styles.createWorkspaceButton}>
+                        <Text style={styles.createWorkspaceButtonText}>Create</Text>
+                    </View>
+                </TouchableHighlight>
+            </View>
+          );
+    }
+}
