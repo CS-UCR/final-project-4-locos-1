ffc37b7d323caae11b6770e4f87db3cdb8684913
Francisco Gallego <fgall002@ucr.edu>
2019-12-02 09:29:51 -0800

    added access code

--- src/models/workspace.js
+++ src/models/workspace.js
@@ -1,11 +1,12 @@
 class Workspace {
 
-    constructor(id, workspaceTitle, authId ,color, imageUri){
+    constructor(id, workspaceTitle, authId ,color, imageUri,accessCode){
         this.id = id;
         this.workspaceTitle = workspaceTitle;
         this.authId = authId
         this.color = color
         this.imageUri = imageUri
+        this.accessCode =  accessCode
     }
 } 
 
