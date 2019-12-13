c99ae7b3300d454d03bfd65cac14ff529880bb55
KevinFrazier <kfraz001@ucr.edu>
2019-12-13 05:39:24 -0800

    fixed Dashboard rendering

--- src/components/dashboardComponents/Accordian.js
+++ src/components/dashboardComponents/Accordian.js
@@ -43,6 +43,9 @@ export default class Accordian extends Component{
           day6DateTxt: '2019-12-11',
           day7DateTxt: '2019-12-12',
         }
+
+
+        this.toggleExpand = this.toggleExpand.bind(this)
     }
 
     getPast7Days(){
@@ -106,93 +109,106 @@ export default class Accordian extends Component{
 
     }
   configurePersonal(){
-
-    console.log("firing getPast7days")
     return(
-                <View>
-                  <View height={20}/>
-                  <View height={20}>
-                  <Text style = {{fontSize: 15, fontWeight: "bold", alignItems: "center", color: Colors.deepPurpleColor, marginTop: 4}}> Last 7 Days In Minutes Spent Studying </Text>
-                  </View>
-                  <View height={200}>
-                  <BarChart
-                      data= {this.getPast7Days()}
-                      width={Dimensions.get('window').width - 2}
-                      height={200}
-                      yAxisLabel={''}
-                      chartConfig={{
-                        backgroundColor: '#FFFFFF',
-                        backgroundGradientFrom: '#FFFFFF',
-                        backgroundGradientTo: '#FFFFFF',
-                        decimalPlaces: 0,
-                        color: (opacity = 1) => `rgba(74, 20, 140, ${opacity})`,
-                        style: {
-                          //borderRadius: 16,
-                        },
-                      }}
-                      style={{
-                        marginVertical: 8,
-                       // borderRadius: 16,
-                      }}
-                    />
-                  </View>
-                  <View height={30}/>
-                  <View height={20}>
-                  <Text style = {{fontSize: 15, fontWeight: "bold", alignItems: "center", color: Colors.deepPurpleColor, marginTop: 0}}> How well did you spread out your time? </Text>
-                  </View>
-                    <View height={200}>
-                  <ContributionGraph
-                      values={[
-                        { date: this.state.day1DateTxt, count: this.state.day1Time },
-                        { date: this.state.day2DateTxt, count: this.state.day2Time },
-                        { date: this.state.day3DateTxt, count: this.state.day3Time },
-                        { date: this.state.day4DateTxt, count: this.state.day4Time },
-                        { date: this.state.day5DateTxt, count: this.state.day5Time },
-                        { date: this.state.day6DateTxt, count: this.state.day6Time },
-                        { date: this.state.day7DateTxt, count: this.state.day7Time },
-                        { date: '2019-11-04', count: 3},
-                        { date: '2019-11-07', count: 30},
-                        { date: '2019-11-10', count: 89},
-                        { date: '2019-11-15', count: 3},
-                        { date: '2019-11-19', count: 35},
-                        { date: '2019-11-24', count: 3},
-                        { date: '2019-11-25', count: 30},
-                        { date: '2019-11-26', count: 89},
-                        { date: '2019-11-28', count: 3},
-                        { date: '2019-11-30', count: 35},
-                        { date: '2019-10-04', count: 3},
-                        { date: '2019-10-07', count: 30},
-                        { date: '2019-10-10', count: 89},
-                        { date: '2019-10-15', count: 3},
-                        { date: '2019-10-16', count: 35},
-                        { date: '2019-10-30', count: 35},
-                        { date: '2019-10-17', count: 3},
-                        { date: '2019-10-14', count: 30},
-                        { date: '2019-10-01', count: 89},
-                        { date: '2019-09-29', count: 3},
-                        { date: '2019-09-30', count: 35},
-                        { date: '2019-09-18', count: 3},
-                        { date: '2019-09-12', count: 35},
-                        { date: '2019-09-04', count: 3},
-                        { date: '2019-09-15', count: 30},
-                      ]}
-                      endDate={new Date(this.state.day7DateTxt)}
-                      numDays={100}
-                      width={Dimensions.get('window').width - 2}
-                      height={200}
-                      chartConfig={{
-                        backgroundColor: '#FFFFFF',
-                        backgroundGradientFrom: '#FFFFFF',
-                        backgroundGradientTo: '#FFFFFF',
-                        color: (opacity = 1) => `rgba(74, 20, 140, ${opacity})`,
-                        strokeWidth: 60,
-                        style: {
-                          //borderRadius: 16,
-                        },
-                      }}
-                    />
-                    </View>
-                </View>
+        <View>
+          <View height={20}/>
+          <View height={20}>
+          <Text style = {{fontSize: 15, fontWeight: "bold", alignItems: "center", color: Colors.deepPurpleColor, marginTop: 4}}> Last 7 Days In Minutes Spent Studying </Text>
+          </View>
+          <View height={200}>
+          <BarChart
+              data={{
+                labels: [
+                  this.state.day1Txt,
+                  this.state.day2Txt,
+                  this.state.day3Txt,
+                  this.state.day4Txt,
+                  this.state.day5Txt,
+                  this.state.day6Txt,
+                  this.state.day7Txt,
+                ],
+                datasets: [
+                  {
+                    data: [this.state.day1Time, this.state.day2Time, this.state.day3Time, this.state.day4Time, this.state.day5Time, this.state.day6Time, this.state.day7Time],
+                  },
+                ],
+              }}
+              width={Dimensions.get('window').width - 2}
+              height={200}
+              yAxisLabel={''}
+              chartConfig={{
+                backgroundColor: '#FFFFFF',
+                backgroundGradientFrom: '#FFFFFF',
+                backgroundGradientTo: '#FFFFFF',
+                decimalPlaces: 0,
+                color: (opacity = 1) => `rgba(74, 20, 140, ${opacity})`,
+                style: {
+                  //borderRadius: 16,
+                },
+              }}
+              style={{
+                marginVertical: 8,
+                // borderRadius: 16,
+              }}
+            />
+          </View>
+          <View height={30}/>
+          <View height={20}>
+          <Text style = {{fontSize: 15, fontWeight: "bold", alignItems: "center", color: Colors.deepPurpleColor, marginTop: 0}}> How well did you spread out your time? </Text>
+          </View>
+            {/* <View height={200}>
+          <ContributionGraph
+              values={[
+                { date: this.state.day1DateTxt, count: this.state.day1Time },
+                { date: this.state.day2DateTxt, count: this.state.day2Time },
+                { date: this.state.day3DateTxt, count: this.state.day3Time },
+                { date: this.state.day4DateTxt, count: this.state.day4Time },
+                { date: this.state.day5DateTxt, count: this.state.day5Time },
+                { date: this.state.day6DateTxt, count: this.state.day6Time },
+                { date: this.state.day7DateTxt, count: this.state.day7Time },
+                { date: '2019-11-04', count: 3},
+                { date: '2019-11-07', count: 30},
+                { date: '2019-11-10', count: 89},
+                { date: '2019-11-15', count: 3},
+                { date: '2019-11-19', count: 35},
+                { date: '2019-11-24', count: 3},
+                { date: '2019-11-25', count: 30},
+                { date: '2019-11-26', count: 89},
+                { date: '2019-11-28', count: 3},
+                { date: '2019-11-30', count: 35},
+                { date: '2019-10-04', count: 3},
+                { date: '2019-10-07', count: 30},
+                { date: '2019-10-10', count: 89},
+                { date: '2019-10-15', count: 3},
+                { date: '2019-10-16', count: 35},
+                { date: '2019-10-30', count: 35},
+                { date: '2019-10-17', count: 3},
+                { date: '2019-10-14', count: 30},
+                { date: '2019-10-01', count: 89},
+                { date: '2019-09-29', count: 3},
+                { date: '2019-09-30', count: 35},
+                { date: '2019-09-18', count: 3},
+                { date: '2019-09-12', count: 35},
+                { date: '2019-09-04', count: 3},
+                { date: '2019-09-15', count: 30},
+              ]}
+              endDate={new Date(this.state.day7DateTxt)}
+              numDays={100}
+              width={Dimensions.get('window').width - 2}
+              height={200}
+              chartConfig={{
+                backgroundColor: '#FFFFFF',
+                backgroundGradientFrom: '#FFFFFF',
+                backgroundGradientTo: '#FFFFFF',
+                color: (opacity = 1) => `rgba(74, 20, 140, ${opacity})`,
+                strokeWidth: 60,
+                style: {
+                  //borderRadius: 16,
+                },
+              }}
+            />
+            </View> */}
+        </View>
     )
   }
 
@@ -203,7 +219,7 @@ export default class Accordian extends Component{
 
     
     //check if type
-
+    console.log("configureRender")
     if(this.state.data.type == "Personal"){
         //render Personal
         return(this.configurePersonal())
@@ -216,27 +232,31 @@ export default class Accordian extends Component{
   render() {
     console.log("rerendering accordian 8")
     return (
-     // <View style= {styles.container}>
+     //<View style= {styles.container}>
        <View style = {{backgroundColor : '#FFFFFF'}}>
-            <TouchableOpacity style={styles.row} onPress={()=>this.toggleExpand()}>
+            {console.log("past view")}
+            <TouchableOpacity style={styles.row} onPress={this.toggleExpand}>
                 <Text style={styles.title}>{this.props.title}</Text>
+                {console.log("past text")}
                 <Icon name={this.state.expanded ? 'keyboard-arrow-up' : 'keyboard-arrow-down'} size={30} color={'#404040'} />
+                {console.log("arrows")}
             </TouchableOpacity>
-            <View style={styles.parentHr}/>
-            {
+            { 
                 this.state.expanded &&
-                <View style = {{flex : 1    }}>
+                <View style = {{flex : 1 }}>
+                    {console.log("beginning")}
                     {this.configureRender()}
                 </View>
             }
        </View>
-     // </View>
+     //</View>
     )
   }
 
   
 
   toggleExpand=()=>{
+    console.log
     this.setState({expanded : !this.state.expanded})
   }
 
